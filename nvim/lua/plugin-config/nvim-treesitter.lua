1ab7cd3cc66e97a556e33fe24ace6573a62a5761
lvsoso <wuqize5109@qq.com>
2022-05-01 23:50:11 +0800

    language format

--- nvim/lua/plugin-config/nvim-treesitter.lua
+++ nvim/lua/plugin-config/nvim-treesitter.lua
@@ -9,12 +9,33 @@ treesitter.setup({
   -- :TSInstallInfo 命令查看支持的语言
   -- all/maintained 一次下载所有的parsers
   -- ensure_installed 确认安装
-  ensure_installed = { "markdown","python","go","json", "html", "css", "vim", "lua", "javascript", "typescript", "tsx" },
+  ensure_installed = { "markdown","python","gomod","go","json", "html", "css", "vim", "lua", "javascript", "typescript", "tsx" },
   -- 启用代码高亮模块
   highlight = {
     enable = true,
     -- 关闭vim 的正则语法高亮
     additional_vim_regex_highlighting = false, 
   },
+
+  -- 启用增量选择模块
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      node_decremental = "<BS>",
+      scope_incremental = "<TAB>",
+    },
+    -- 启用代码缩进模块 (=)
+    indent = {
+      enable = true,
+    },
+  },
 })
 
+-- 开启 Folding 模块
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- 默认不要折叠
+-- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
+vim.opt.foldlevel = 99
